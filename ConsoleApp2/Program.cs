c05f715ea95fa4312660db2e064608582514129d
Jan Boerman <Janboerman95@gmail.com>
2018-03-15 15:21:47 +0100

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new HelloWorld().GetHelloWorld());
+        }
+
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    public class Foo
+    {
+        public String Bar(int lol)
+        {
+            return new HelloWorld().GetHelloWorld();
+        }
+    }
+
+    public class Bar
+    {
+        public String Foo(Foo iksDee)
+        {
+            return iksDee.Bar(0);
+        }
+    }
+
+    public class HelloWorld
+    {
+        public virtual String GetHelloWorld()
+        {
+            return "Hello, World!";
+        }
+    }
+
+
+    public class Triangle
+    {
+        public readonly int x, y, z;
+
+        public Triangle(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        
+        public TriangleType getTriangleType()
+        {
+            if (x <= 0 || y <= 0 || z <= 0) return TriangleType.INVALID;
+            if (x + y < z || z + x < y || y + z < x) return TriangleType.INVALID;
+            if (x == y && y == z) return TriangleType.EQUILITERAL;
+            if (x == y || y == z || z == x) return TriangleType.ISOSCELES;
+
+            return TriangleType.SCALENE;
+        }
+    }
+
+    public enum TriangleType
+    {
+        SCALENE,
+        ISOSCELES,
+        EQUILITERAL,
+        INVALID,
+    }
+
+}
---
+++ ConsoleApp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("664cec7a-9978-4f85-b87c-69ca34dbfff2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("ConsoleApp2.Tests")]
+[assembly: InternalsVisibleTo("PlayingWithIntelliTest.Explorables")]
+[assembly: InternalsVisibleTo("ConsoleApp2.Tests1")]
+[assembly: InternalsVisibleTo("ConsoleApp2.Tests2")]
+[assembly: InternalsVisibleTo("Program.Tests")]
+[assembly: InternalsVisibleTo("Program.Tests1")]
+
